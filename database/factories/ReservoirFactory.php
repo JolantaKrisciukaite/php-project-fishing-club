311300922c5c5c21acfb8f31c049df11aa9612ff
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-07-31 17:52:54 +0300

    Initial commit

---
+++ database/factories/ReservoirFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Reservoir;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ReservoirFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Reservoir::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2021_07_31_144829_create_members_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('surname', 150);
+            $table->string('live', 50);
+            $table->integer('experience');
+            $table->integer('registered');
+            $table->unsignedBigInteger('reservoir_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
