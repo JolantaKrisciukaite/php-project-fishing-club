9bbb754659fda6f2c73520264d3bce2a55480af3
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-07-31 19:00:13 +0300

    a

---
+++ resources/views/reservoir/create.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-sm-12 col-md-10 col-lg-8 col-xl-6">
+                <div class="card">
+
+                    <h3 class="titleHorses">Create new reservoir</h3>
+                   
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('reservoir.store') }}">
+
+                            <div class="form-group">
+                                <label>Title:</label>
+                                <input placeholder="Enter reservoir title" type="text" name="reservoir_title" class="form-control"
+                                    value="{{ old('reservoir_title') }}">
+                            </div>
+
+                            <div class="form-group">
+                                <label>Area:</label>
+                                <input placeholder="Enter info about reservoir" type="text" name="reservoir_area"
+                                    class="form-control" value="{{ old('reservoir_area') }}">
+                            </div>
+
+                            <div class="form-group">
+                                <label>About:</label>
+                                <textarea id="summernote" placeholder="Enter info about reservoir" type="text"
+                                    name="reservoir_about" class="form-control" value="{{ old('reservoir_about') }}"></textarea>
+                            </div>
+
+                            @csrf
+                            <button class="addButton" type="submit">Add</button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function() {
+            $('#summernote').summernote();
+        });
+    </script>
+
+@endsection
