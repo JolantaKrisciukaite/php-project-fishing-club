30f94cd4f63be715151c5e07bf665bd3842a8858
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-08-02 20:26:16 +0300

    a

--- storage/framework/views/3a7d02a8dab4567a4d9fd0cb326a83cde7321656.php
+++ storage/framework/views/3a7d02a8dab4567a4d9fd0cb326a83cde7321656.php
@@ -9,13 +9,13 @@
                     <div class="card-body">
                         <div class="card-show">
                             <div class="masters">
-                                <h6 class="h6">Reservoir title: <?php echo e($reservoir->title); ?></h6>
+                                <h6>Reservoir title: <?php echo e($reservoir->title); ?></h6>
                             </div>
                             <div class="masters">
-                                <h6 class="h6">Reservoir area: <?php echo e($reservoir->area); ?> (km2)</h6>
+                                <h6>Reservoir area: <?php echo e($reservoir->area); ?> (km2)</h6>
                             </div>
                             <div class="masters">
-                                <h6 class="h6">Reservoir about: <?php echo $reservoir->about; ?></h6>
+                                <h6>Reservoir about: <?php echo $reservoir->about; ?></h6>
                             </div>
                         </div>
 
