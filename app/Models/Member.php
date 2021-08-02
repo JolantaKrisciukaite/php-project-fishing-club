b6772e57c53e0e7c6c8bbf1de9645e2a9e95122e
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-08-02 15:15:24 +0300

    a

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
+    use HasFactory;
+    
     public function memberReservoir()
     {
         return $this->belongsTo('App\Models\Reservoir', 'reservoir_id', 'id');
